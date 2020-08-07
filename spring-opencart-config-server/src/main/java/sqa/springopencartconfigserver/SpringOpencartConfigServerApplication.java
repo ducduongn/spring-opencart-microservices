ebf0da1c6d3f5ddd6469e00d6deaf79e6b657c1b
ducduongn <48018619+ducduongn@users.noreply.github.com>
2020-08-08 01:12:49 +0700

    add config server'

---
+++ spring-opencart-config-server/src/main/java/sqa/springopencartconfigserver/SpringOpencartConfigServerApplication.java
@@ -0,0 +1,13 @@
+package sqa.springopencartconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOpencartConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOpencartConfigServerApplication.class, args);
+	}
+
+}
---
+++ spring-opencart-config-server/src/test/java/sqa/springopencartconfigserver/SpringOpencartConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sqa.springopencartconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOpencartConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
